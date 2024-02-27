eec30c54a2e5ee70e0043d62b37ed81d2815b40a
shenyichong <shenyichong2003@163.com>
2024-02-27 06:42:30 +0000

    update

--- openfold/scripts/download_alphafold_params.sh
+++ openfold/scripts/download_alphafold_params.sh
@@ -34,7 +34,7 @@ ROOT_DIR="${DOWNLOAD_DIR}/params"
 SOURCE_URL="https://storage.googleapis.com/alphafold/alphafold_params_2022-01-19.tar"
 BASENAME=$(basename "${SOURCE_URL}")
 
-mkdir --parents "${ROOT_DIR}"
+mkdir --parents "${ROOT_DIR}" 
 aria2c "${SOURCE_URL}" --dir="${ROOT_DIR}"
 tar --extract --verbose --file="${ROOT_DIR}/${BASENAME}" \
   --directory="${ROOT_DIR}" --preserve-permissions
